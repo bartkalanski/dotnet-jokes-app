7a441538e9aba4a04ff4d61d6a615a580fb34852
Bart Kalanski <bkalanski@gmail.com>
2022-01-04 08:40:25 +0000

    Big commit

---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
